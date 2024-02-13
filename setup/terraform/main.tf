61e9c6a8fd01f26ff31ea187ae2b1aa27e1b9de5
sidahmed-faisal <sidahmedfaisal.sf@gmail.com>
2024-02-13 13:16:17 +0400

    Add resources using terraform

--- setup/terraform/main.tf
+++ setup/terraform/main.tf
@@ -113,28 +113,6 @@ resource "aws_vpc_endpoint" "ecr-api-endpoint" {
   private_dns_enabled = true
 }
 
-###################
-# ECR Repositories
-###################
-resource "aws_ecr_repository" "frontend" {
-  name                 = "frontend"
-  image_tag_mutability = "MUTABLE"
-  force_delete         = true
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
-
-resource "aws_ecr_repository" "backend" {
-  name                 = "backend"
-  image_tag_mutability = "MUTABLE"
-  force_delete         = true
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
 
 ################
 # EKS Resources
@@ -252,79 +230,24 @@ data "aws_iam_policy_document" "assume_role_policy" {
   }
 }
 
-######################
-# CodeBuild Resources
-######################
-# Create a CodeBuild project
-resource "aws_codebuild_project" "codebuild" {
-  name          = "udacity"
-  description   = "Udacity CodeBuild project"
-  service_role  = aws_iam_role.codebuild.arn
-  build_timeout = 60
-  artifacts {
-    type = "NO_ARTIFACTS"
-  }
-
-  environment {
-    compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/standard:5.0"
-    type                        = "LINUX_CONTAINER"
-    image_pull_credentials_type = "CODEBUILD"
-    privileged_mode             = true
-  }
-
-  source {
-    type            = "GITHUB"
-    location        = "https://github.com/your-org/your-repo"
-    git_clone_depth = 1
-    buildspec       = "buildspec.yml"
-  }
-
-  cache {
-    type = "NO_CACHE"
-  }
-}
-
-# Create the Codebuild Role
-resource "aws_iam_role" "codebuild" {
-  name = "codebuild-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "codebuild.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
-
-# Attach the IAM policy to the codebuild role
-resource "aws_iam_role_policy_attachment" "codebuild" {
-  policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildAdminAccess"
-  role       = aws_iam_role.codebuild.name
-}
-
 ####################
 # Github Action role
 ####################
 resource "aws_iam_user" "github_action_user" {
   name = "github-action-user"
 }
-
-resource "aws_iam_user_policy" "github_action_user_permission" {
-  user   = aws_iam_user.github_action_user.name
-  policy = data.aws_iam_policy_document.github_policy.json
-}
-
 data "aws_iam_policy_document" "github_policy" {
   statement {
     effect    = "Allow"
     actions   = ["ecr:*", "eks:*", "ec2:*"]
     resources = ["*"]
   }
+}
+resource "aws_iam_policy" "github_policy" {
+  name   = "github-policy"
+  policy = data.aws_iam_policy_document.github_policy.json
+}
+resource "aws_iam_user_policy_attachment" "github_policy" {
+  user       = aws_iam_user.github_action_user.name
+  policy_arn = aws_iam_policy.github_policy.arn
 }
--- setup/terraform/outputs.tf
+++ setup/terraform/outputs.tf
@@ -1,10 +1,3 @@
-output "frontend_ecr" {
-  value = aws_ecr_repository.frontend.repository_url
-}
-output "backend_ecr" {
-  value = aws_ecr_repository.backend.repository_url
-}
-
 output "cluster_name" {
   value = aws_eks_cluster.main.name
 }
